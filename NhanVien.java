64e0ae874a6b039a6e69d36a4242abf04b2a827a
anhtaingo961 <nguyendinhtuan25042004@gmail.com>
2024-03-21 15:41:55 +0700

    sonngu

---
+++ NhanVien.java
@@ -0,0 +1,54 @@
+package bai3;
+
+public class NhanVien {
+private String tenNhanVien;
+private double luongCoBan;
+private double heSoLuong;
+private double LUONG_MAX;
+public NhanVien(String tenNhanVien,double heSoLuong) {
+	this.tenNhanVien=tenNhanVien;
+	this.heSoLuong=heSoLuong;
+}
+public String gettenNhanVien(){
+	return this.tenNhanVien;
+}
+public void settenNhanVien(String tenNhanVien) {
+	this.tenNhanVien=tenNhanVien;
+}
+public double getluongCoBan(){
+	return this.luongCoBan;
+}
+public void setluongCoBan(double luongCoBan) {
+	this.luongCoBan= luongCoBan;
+}
+public double getheSoLuong() {
+	return this.heSoLuong;
+}
+public void setheSoLuong(double heSoLuong) {
+	this.heSoLuong=heSoLuong;
+}
+public double getLUONG_MAX() {
+	return this.LUONG_MAX;
+}
+public void setLUONG_MAX(double LUONG_MAX) {
+	this.LUONG_MAX=LUONG_MAX;
+}
+public boolean tangLuong(double n) {
+	if(luongCoBan*(heSoLuong+n)>LUONG_MAX) {
+		return false;
+	}
+	else {
+		heSoLuong +=n;
+	return true;
+	}
+
+}
+public double tinhLuong() {
+	 return luongCoBan*heSoLuong <LUONG_MAX? luongCoBan*heSoLuong:LUONG_MAX;
+}
+public void inTTin() {
+    System.out.println();
+    System.out.println("Ten nhan vien: " + tenNhanVien);
+    System.out.println("Luong: " + tinhLuong());
+}
+}
---
+++ TestNV.java
@@ -0,0 +1,21 @@
+package bai3;
+
+import java.util.Scanner;
+
+public class TestNV {
+public static void main(String[] args) {
+	Scanner sc = new Scanner(System.in);
+	System.out.println("nhap ten nhan vien va he so luong");
+	NhanVien a=new NhanVien(sc.next(),sc.nextDouble());
+	System.out.println("nhap luong max:");
+	a.setLUONG_MAX(sc.nextDouble());
+	System.out.println("nhap luong co ban");
+	a.setluongCoBan(sc.nextDouble());
+	System.out.println("nhap he so tang luong va ktra xem co tang khong");
+	a.tangLuong(sc.nextDouble());
+	double b= a.tinhLuong();
+	System.out.println("ten nhan vien "+a.gettenNhanVien());
+	System.out.println("luong max"+a.getLUONG_MAX());
+	System.out.println(b);
+}
+}
